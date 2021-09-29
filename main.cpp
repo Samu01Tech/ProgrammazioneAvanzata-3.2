ac81840a221f005fb2774cacbfd3f01e7f836965
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-09-29 15:16:26 +0000

    b == b

--- main.cpp
+++ main.cpp
@@ -19,17 +19,28 @@ class B{
     B(){
         val = 1;
     }
+    //Re: b == b
+    bool operator ==(const B& b){
+        return val == b.val;
+    }
     friend bool operator ==(const A& a, const B& b);
 };
 
+//Re: a == b
 bool operator ==(const A& a, const B& b){
     return a.val == b.val;
 }
+//Re: b == a
+bool operator ==(const B& b, const A& a){
+    return a == b;
+}
+
 
 int main() {
     A a;
     B b;
     if(a == b) cout << "a == b" << endl;
-    //if (b == a) cout << "b == a" << endl;
+    if (b == a) cout << "b == a" << endl;
+    if (b == b) cout << "b == b" << endl;
     return 0;
 }
